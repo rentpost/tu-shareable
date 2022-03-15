cd4aceae853a0aeccd0cdaf57c1d4bacd1b10fb5
Pekka Laiho <pekka.i.laiho@gmail.com>
2022-03-15 16:22:59 +0700

    Landlord class and tests

--- src/Model/Landlord.php
+++ src/Model/Landlord.php
@@ -4,17 +4,110 @@ declare(strict_types = 1);
 
 namespace Rentpost\TUShareable\Model;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * Class that represents a landlord.
  */
 class Landlord
 {
 
+    use ValidateTrait;
+
+
+    protected ?int $id = null;
+
+    protected Email $emailAddress;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50)]
+    protected string $firstName;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50)]
+    protected string $lastName;
+
+    protected Phone $phone;
+
+    protected string $businessName;
+
+    protected Address $businessAddress;
+
+    #[Assert\IsTrue(message: 'Terms and conditions need to be accepted.')]
+    protected bool $acceptedTermsAndConditions;
+
 
     public function __construct(
-        protected Logger $logger,
-        protected HttpClient $http
-    ) { }
+        Email $emailAddress,
+        string $firstName,
+        string $lastName,
+        Phone $phone,
+        string $businessName,
+        Address $businessAddress,
+        bool $acceptedTermsAndConditions
+    ) {
+        $this->emailAddress = $emailAddress;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->phone = $phone;
+        $this->businessName = $businessName;
+        $this->businessAddress = $businessAddress;
+        $this->acceptedTermsAndConditions = $acceptedTermsAndConditions;
+
+        $this->validate();
+    }
 
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getEmail(): Email
+    {
+        return $this->emailAddress;
+    }
+
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+
+    public function getPhone(): Phone
+    {
+        return $this->phone;
+    }
+
+
+    public function getBusinessName(): string
+    {
+        return $this->businessName;
+    }
+
+
+    public function getBusinessAddress(): Address
+    {
+        return $this->businessAddress;
+    }
+
+
+    public function getAcceptedTermsAndConditions(): bool
+    {
+        return $this->acceptedTermsAndConditions;
+    }
+
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
 }
---
+++ test/Model/LandlordTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+use Rentpost\TUShareable\Model\Address;
+use Rentpost\TUShareable\Model\Email;
+use Rentpost\TUShareable\Model\Landlord;
+use Rentpost\TUShareable\Model\Phone;
+use Rentpost\TUShareable\ValidationException;
+
+class LandlordTest extends TestCase
+{
+
+    public function testConstructorAndGetters()
+    {
+        $landlord = $this->makeObject('First', 'Last', 'Business', true);
+
+        $this->assertSame('First', $landlord->getFirstName());
+        $this->assertSame('Last', $landlord->getLastName());
+        $this->assertSame('Business', $landlord->getBusinessName());
+        $this->assertTrue($landlord->getAcceptedTermsAndConditions());
+
+        $this->assertInstanceOf(Address::class, $landlord->getBusinessAddress());
+        $this->assertInstanceOf(Email::class, $landlord->getEmail());
+        $this->assertInstanceOf(Phone::class, $landlord->getPhone());
+    }
+
+
+    public function validationProvider()
+    {
+        return [
+            // No first name
+            [ '', 'Last', 'Business', true, 'This value should not be blank.' ],
+            // No last name
+            [ 'First', '', 'Business', true, 'This value should not be blank.' ],
+            // Not accepted terms
+            [ 'First', 'Last', 'Business', false, 'Terms and conditions need to be accepted.' ],
+        ];
+    }
+
+
+    /**
+     * @dataProvider validationProvider
+     */
+    public function testValidationErrors(string $firstName, string $lastName, string $businessName, bool $acceptedTerms, string $errorMessage)
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage($errorMessage);
+
+        $this->makeObject($firstName, $lastName, $businessName, $acceptedTerms);
+    }
+
+
+    protected function makeObject(string $firstName, string $lastName, string $businessName, bool $acceptedTerms = true): Landlord
+    {
+        // Address, Email and Phone are tested separately. Here we just give
+        // them valid values for now and focus on testing other properties of Landlord.
+
+        $address = new Address('Streetname', 'Apartment', '', '', 'Los Angeles', 'CA', '12345');
+        $email = new Email('test@example.com');
+        $phone = new Phone('0123456789', 'Home');
+
+        return new Landlord(
+            $email,
+            $firstName,
+            $lastName,
+            $phone,
+            $businessName,
+            $address,
+            $acceptedTerms
+        );
+    }
+}
