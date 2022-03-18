f9ec18e9e763b420cc20a5134a65b8a2f8629da0
Pekka Laiho <pekka.i.laiho@gmail.com>
2022-03-18 07:21:23 +0700

    Person class

---
+++ src/Model/Person.php
@@ -0,0 +1,139 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Rentpost\TUShareable\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class that represents a person.
+ */
+class Person
+{
+
+    use Validate;
+
+
+    protected ?int $id = null;
+
+    #[Assert\NotBlank]
+    protected Email $emailAddress;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 15)]
+    protected string $firstName;
+
+    #[Assert\Length(min: 1, max: 15)]
+    protected ?string $middleName;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 25)]
+    protected string $lastName;
+
+    #[Assert\NotBlank]
+    protected Phone $phone;
+
+    #[Assert\NotBlank]
+    protected SocialSecurityNumber $socialSecurityNumber;
+
+    #[Assert\NotBlank]
+    protected Date $dateOfBirth;
+
+    #[Assert\NotBlank]
+    protected Address $homeAddress;
+
+    #[Assert\IsTrue(message: 'Terms and conditions need to be accepted.')]
+    protected bool $acceptedTermsAndConditions;
+
+
+    public function __construct(
+        Email $emailAddress,
+        string $firstName,
+        ?string $middleName,
+        string $lastName,
+        Phone $phone,
+        SocialSecurityNumber $socialSecurityNumber,
+        Date $dateOfBirth,
+        Address $homeAddress,
+        bool $acceptedTermsAndConditions
+    ) {
+        $this->emailAddress = $emailAddress;
+        $this->firstName = $firstName;
+        $this->middleName = $middleName;
+        $this->lastName = $lastName;
+        $this->phone = $phone;
+        $this->socialSecurityNumber = $socialSecurityNumber;
+        $this->dateOfBirth = $dateOfBirth;
+        $this->homeAddress = $homeAddress;
+        $this->acceptedTermsAndConditions = $acceptedTermsAndConditions;
+
+        $this->validate();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getEmail(): Email
+    {
+        return $this->emailAddress;
+    }
+
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+
+    public function getMiddleName(): ?string
+    {
+        return $this->middleName;
+    }
+
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+
+    public function getPhone(): Phone
+    {
+        return $this->phone;
+    }
+
+
+    public function getSocialSecurityNumber(): SocialSecurityNumber
+    {
+        return $this->socialSecurityNumber;
+    }
+
+
+    public function getDateOfBirth(): Date
+    {
+        return $this->dateOfBirth;
+    }
+
+
+    public function getHomeAddress(): Address
+    {
+        return $this->homeAddress;
+    }
+
+
+    public function getAcceptedTermsAndConditions(): bool
+    {
+        return $this->acceptedTermsAndConditions;
+    }
+
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+}
---
+++ test/Model/PersonTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+use Rentpost\TUShareable\Model\Address;
+use Rentpost\TUShareable\Model\Date;
+use Rentpost\TUShareable\Model\Email;
+use Rentpost\TUShareable\Model\Person;
+use Rentpost\TUShareable\Model\Phone;
+use Rentpost\TUShareable\Model\SocialSecurityNumber;
+use Rentpost\TUShareable\ValidationException;
+
+class PersonTest extends TestCase
+{
+
+    public function testConstructorAndGetters()
+    {
+        $person = $this->makeObject('First', 'Middle', 'Last', true);
+
+        $this->assertSame('First', $person->getFirstName());
+        $this->assertSame('Middle', $person->getMiddleName());
+        $this->assertSame('Last', $person->getLastName());
+        $this->assertTrue($person->getAcceptedTermsAndConditions());
+
+        $this->assertInstanceOf(Date::class, $person->getDateOfBirth());
+        $this->assertInstanceOf(Email::class, $person->getEmail());
+        $this->assertInstanceOf(Address::class, $person->getHomeAddress());
+        $this->assertInstanceOf(Phone::class, $person->getPhone());
+        $this->assertInstanceOf(SocialSecurityNumber::class, $person->getSocialSecurityNumber());
+    }
+
+
+    public function validationProvider()
+    {
+        return [
+            // No first name
+            [ '', 'Middle', 'Last', true, 'This value should not be blank.' ],
+            // No last name
+            [ 'First', 'Middle', '', true, 'This value should not be blank.' ],
+            // Not accepted terms
+            [ 'First', 'Middle', 'Last', false, 'Terms and conditions need to be accepted.' ],
+        ];
+    }
+
+
+    /**
+     * @dataProvider validationProvider
+     */
+    public function testValidationErrors(string $firstName, ?string $middleName, string $lastName, bool $acceptedTerms, string $errorMessage)
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage($errorMessage);
+
+        $this->makeObject($firstName, $middleName, $lastName, $acceptedTerms);
+    }
+
+
+    protected function makeObject(string $firstName, ?string $middleName, string $lastName, bool $acceptedTerms = true): Person
+    {
+        // Address, Date, Email, Phone and SocialSecurityNumber are tested separately. Here we just give
+        // them valid values for now and focus on testing other properties of Person.
+
+        $address = new Address('Streetname', 'Apartment', '', '', 'Los Angeles', 'CA', '12345');
+        $email = new Email('test@example.com');
+        $phone = new Phone('0123456789', 'Home');
+        $ssn = new SocialSecurityNumber('012345789');
+        $date = new Date('1990-03-15');
+
+        return new Person(
+            $email,
+            $firstName,
+            $middleName,
+            $lastName,
+            $phone,
+            $ssn,
+            $date,
+            $address,
+            $acceptedTerms
+        );
+    }
+}
