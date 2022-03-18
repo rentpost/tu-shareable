80a7b80b6e824eee90dd9d8cd0c500a3e684f424
Pekka Laiho <pekka.i.laiho@gmail.com>
2022-03-18 08:25:11 +0700

    Renter class

---
+++ src/Model/Renter.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Rentpost\TUShareable\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class that represents a renter.
+ */
+class Renter
+{
+
+    use Validate;
+
+
+    #[Assert\NotBlank]
+    protected Person $person;
+
+    #[Assert\NotBlank]
+    protected Money $income;
+
+    #[Assert\NotBlank]
+    #[Assert\Choice(['PerMonth', 'PerYear'])]
+    protected string $incomeFrequency;
+
+    #[Assert\NotBlank]
+    protected Money $otherIncome;
+
+    #[Assert\NotBlank]
+    #[Assert\Choice(['PerMonth', 'PerYear'])]
+    protected string $otherIncomeFrequency;
+
+    #[Assert\NotBlank]
+    protected Money $assets;
+
+    #[Assert\NotBlank]
+    #[Assert\Choice(['NotEmployed', 'Employed', 'SelfEmployed', 'Student'])]
+    protected string $employmentStatus;
+
+    protected ?Date $multiShareExpirationDate;
+
+
+    public function __construct(
+        Person $person,
+        Money $income,
+        string $incomeFrequency,
+        Money $otherIncome,
+        string $otherIncomeFrequency,
+        Money $assets,
+        string $employmentStatus,
+        ?Date $multiShareExpirationDate
+    ) {
+        $this->person = $person;
+        $this->income = $income;
+        $this->incomeFrequency = $incomeFrequency;
+        $this->otherIncome = $otherIncome;
+        $this->otherIncomeFrequency = $otherIncomeFrequency;
+        $this->assets = $assets;
+        $this->employmentStatus = $employmentStatus;
+        $this->multiShareExpirationDate = $multiShareExpirationDate;
+
+        $this->validate();
+    }
+
+
+    public function getPerson(): Person
+    {
+        return $this->person;
+    }
+
+
+    public function getIncome(): Money
+    {
+        return $this->income;
+    }
+
+
+    public function getIncomeFrequency(): string
+    {
+        return $this->incomeFrequency;
+    }
+
+
+    public function getOtherIncome(): Money
+    {
+        return $this->otherIncome;
+    }
+
+
+    public function getOtherIncomeFrequency(): string
+    {
+        return $this->otherIncomeFrequency;
+    }
+
+
+    public function getAssets(): Money
+    {
+        return $this->assets;
+    }
+
+
+    public function getEmploymentStatus(): string
+    {
+        return $this->employmentStatus;
+    }
+
+
+    public function getMultiShareExpirationDate(): ?Date
+    {
+        return $this->multiShareExpirationDate;
+    }
+}
---
+++ test/Model/RenterTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+use Rentpost\TUShareable\Model\Address;
+use Rentpost\TUShareable\Model\Date;
+use Rentpost\TUShareable\Model\Email;
+use Rentpost\TUShareable\Model\Money;
+use Rentpost\TUShareable\Model\Person;
+use Rentpost\TUShareable\Model\Phone;
+use Rentpost\TUShareable\Model\Renter;
+use Rentpost\TUShareable\Model\SocialSecurityNumber;
+use Rentpost\TUShareable\ValidationException;
+
+class RenterTest extends TestCase
+{
+
+    public function testConstructorAndGetters()
+    {
+        $renter = $this->makeObject('1000', 'PerMonth', '5000', 'PerYear', '12000', 'SelfEmployed', new Date('2022-12-30'));
+
+        $this->assertInstanceOf(Date::class, $renter->getMultiShareExpirationDate());
+        $this->assertInstanceOf(Money::class, $renter->getIncome());
+        $this->assertInstanceOf(Money::class, $renter->getOtherIncome());
+        $this->assertInstanceOf(Money::class, $renter->getAssets());
+
+        $this->assertSame('1000', $renter->getIncome()->getValue());
+        $this->assertSame('5000', $renter->getOtherIncome()->getValue());
+        $this->assertSame('12000', $renter->getAssets()->getValue());
+
+        $this->assertSame('PerMonth', $renter->getIncomeFrequency());
+        $this->assertSame('PerYear', $renter->getOtherIncomeFrequency());
+        $this->assertSame('2022-12-30', $renter->getMultiShareExpirationDate()->getValue());
+    }
+
+
+    public function validationProvider()
+    {
+        return [
+            // Invalid income frequency
+            ['1000', 'Invalid', '5000', 'PerYear', '12000', 'SelfEmployed', null,
+                'The value you selected is not a valid choice.', 'incomeFrequency'],
+            // Invalid other income frequency
+            ['1000', 'PerMonth', '5000', 'Invalid', '12000', 'SelfEmployed', null,
+                'The value you selected is not a valid choice.', 'otherIncomeFrequency'],
+            // Invalid employment status
+            ['1000', 'PerMonth', '5000', 'PerYear', '12000', 'Invalid', null,
+                'The value you selected is not a valid choice.', 'employmentStatus'],
+        ];
+    }
+
+
+    /**
+     * @dataProvider validationProvider
+     */
+    public function testValidationErrors(string $income, string $incomeFrequency, string $otherIncome,
+        string $otherIncomeFrequency, string $assets, string $employmentStatus,
+        ?Date $multiShareExpirationDate, string $errorMessage, string $fieldname)
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage($errorMessage);
+        $this->expectExceptionMessage(".$fieldname");
+
+        $this->makeObject($income, $incomeFrequency, $otherIncome, $otherIncomeFrequency, $assets,
+            $employmentStatus, $multiShareExpirationDate);
+    }
+
+
+    protected function makePerson(): Person
+    {
+        $address = new Address('Streetname', 'Apartment', '', '', 'Los Angeles', 'CA', '12345');
+        $email = new Email('test@example.com');
+        $phone = new Phone('0123456789', 'Home');
+        $ssn = new SocialSecurityNumber('012345789');
+        $date = new Date('1990-03-15');
+
+        return new Person(
+            $email,
+            'First',
+            'Middle',
+            'Last',
+            $phone,
+            $ssn,
+            $date,
+            $address,
+            true
+        );
+    }
+
+
+    protected function makeObject(string $income, string $incomeFrequency, string $otherIncome,
+        string $otherIncomeFrequency, string $assets, string $employmentStatus,
+        ?Date $multiShareExpirationDate = null): Renter
+    {
+        return new Renter(
+            $this->makePerson(),
+            new Money($income),
+            $incomeFrequency,
+            new Money($otherIncome),
+            $otherIncomeFrequency,
+            new Money($assets),
+            $employmentStatus,
+            $multiShareExpirationDate
+        );
+    }
+}
